5f13de8fce0d5eb659aea19eef7d1967de67a48f
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-12-24 13:06:43 -0300

    feat(string): create chars_to_string

---
+++ src/string.rs
@@ -0,0 +1,30 @@
+use windows::Win32::Foundation::CHAR;
+
+/// Converts a list of char to a Rust String.
+///
+/// # Example
+///
+/// ```
+/// use windows::Win32::Foundation::CHAR;
+/// use batota::string::chars_to_string;
+/// let chars = [CHAR(104), CHAR(101), CHAR(108), CHAR(108), CHAR(111), CHAR(0)];
+/// assert_eq!(chars_to_string(&chars), "hello".to_owned());
+/// ```
+pub fn chars_to_string(chars: &[CHAR]) -> String {
+    let name_ends_at_index = {
+        let mut i = 0;
+
+        while i < chars.len() && chars[i].0 != 0 {
+            i += 1;
+        }
+
+        i
+    };
+
+    let s: Vec<u8> = chars[0..name_ends_at_index]
+        .into_iter()
+        .map(|char| char.0)
+        .collect();
+
+    String::from_utf8_lossy(s.as_ref()).to_string()
+}
